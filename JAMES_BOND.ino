e6f0fccc7b78840ac5133d71beebd1525872c702
colinborins <colinborins@Colins-MacBook-Air.local>
2015-02-12 03:24:27 -0500

    James bond
    James bond Arduino melody project

---
+++ JAMES_BOND.ino
@@ -0,0 +1,46 @@
+#include "pitches.h"
+
+int melody[] = 
+{
+NOTE_G4,NOTE_G4,NOTE_G4,NOTE_G4,NOTE_G4,NOTE_G4,NOTE_G4,NOTE_G4,NOTE_G4,
+   NOTE_B3,NOTE_G3,NOTE_C4,NOTE_G3,NOTE_CS4,NOTE_G3,NOTE_C4,NOTE_G3,NOTE_B3,NOTE_G3,NOTE_C4,NOTE_G3,NOTE_CS4,NOTE_G3,NOTE_C4,NOTE_G3,
+   NOTE_E4,NOTE_F4,NOTE_F4,NOTE_F4,NOTE_F4,NOTE_E4,NOTE_E4,NOTE_E4,
+   NOTE_E4,NOTE_G4,NOTE_G4,NOTE_G4,NOTE_G4,NOTE_E4,NOTE_E4,NOTE_E4,NOTE_E4,NOTE_F4,NOTE_F4,NOTE_F4,NOTE_F4,NOTE_E4,NOTE_E4,NOTE_E4,
+  NOTE_E4,NOTE_G4,NOTE_G4,NOTE_G4,NOTE_G4,NOTE_E4,NOTE_E4,NOTE_E4,
+  NOTE_E4,NOTE_F4,NOTE_F4,NOTE_F4,NOTE_F4,NOTE_E4,NOTE_E4,NOTE_E4,
+  NOTE_E4,NOTE_G4,NOTE_G4,NOTE_G4,NOTE_G4,NOTE_E4,NOTE_E4,NOTE_E4,
+  NOTE_DS5,NOTE_D5,NOTE_B4,NOTE_A4,NOTE_B4,
+  NOTE_E4,NOTE_G4,NOTE_DS5,NOTE_D5,NOTE_G4,NOTE_B4,
+  NOTE_B4,NOTE_FS5,NOTE_F5,NOTE_B4,NOTE_D5,NOTE_AS5,
+  NOTE_A5,NOTE_F5,NOTE_A5,NOTE_DS6,NOTE_D6
+};
+
+int noteDurations[] = {
+8,8,2,8,8,2,16,8,16,8,8,
+   2,4,2,4,2,4,2,4,2,4,2,4,2,4,2,4,
+   8,16,16,8,4,8,8,8,
+   8,16,16,8,4,8,8,8,8,
+   16,16,8,4,8,8,8,
+  8,16,16,8,4,8,8,8,
+  8,16,16,8,4,8,8,8,
+  8,16,16,8,4,8,8,8,
+  8,2,8,8,1,
+  8,4,8,4,8,8,
+  8,8,4,8,4,8,
+  4,8,4,8,3
+};
+
+void setup() {
+
+  for (int thisNote = 0; thisNote < 100; thisNote++) {
+
+   
+    int noteDuration = 1000 / noteDurations[thisNote];
+    tone(13, melody[thisNote], noteDuration);
+    int pauseBetweenNotes = noteDuration * 1;
+    delay(pauseBetweenNotes);
+    noTone(13);
+  }
+}
+
+void loop() { }
